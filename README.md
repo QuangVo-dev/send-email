207fef78bbf3f5f04ee39be07c709ed7ccb6c132
Quang Vo <vothanhtruongquang@gmail.com>
2019-06-14 10:50:36 +0700

    push npm

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # send-email
 Send Email Modules
+
+
+const sendService = new MailService({
+        sendgrid: {
+            apiKey: 'sendgrid-apiKey'
+        },
+        mandrill: {
+            apiKey: 'sendgrid-apiKey'
+        },
+});
--- dist/index.js
+++ dist/index.js
@@ -1,43 +1,47 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const sendgrid_1 = require("./sendgrid/sendgrid");
-class MailService {
-    constructor(config) {
-        this.sendEmailWithService = (service, apiKey, data) => {
-            if (service == 'sendgrid') {
-                return sendgrid_1.sendEmailWithSendgrid(apiKey, data);
-            }
-            if (service == 'mailchimp') {
-            }
-            throw new Error('Mail service currenly not supported');
-        };
-        this.sendEmail = (inputs) => {
-            const msg = {
-                from: inputs.from,
-                to: inputs.to,
-                subject: inputs.subject,
-                content: inputs.content
+var QuangLibrary;
+(function (QuangLibrary) {
+    class MailService {
+        constructor(config) {
+            this.sendEmailWithService = (service, apiKey, data) => {
+                if (service == 'sendgrid') {
+                    return sendgrid_1.sendEmailWithSendgrid(apiKey, data);
+                }
+                if (service == 'mailchimp') {
+                }
+                throw new Error('Mail service currenly not supported');
             };
-            for (let i in this.config) {
-                if (this.config[i].apiKey.length == 0)
-                    continue;
-                else
-                    this.sendEmailWithService(i, this.config[i].apiKey, msg);
-            }
-        };
-        this.config = {
-            mailchimp: {
-                apiKey: ''
-            },
-            sendgrid: {
-                apiKey: ''
-            }
-        };
-        if (Object.values(config).length > 0)
-            this.config = Object.assign({}, this.config, config);
-        else
-            throw new Error('Config file should be an object with services');
+            this.sendEmail = (inputs) => {
+                const msg = {
+                    from: inputs.from,
+                    to: inputs.to,
+                    subject: inputs.subject,
+                    content: inputs.content
+                };
+                for (let i in this.config) {
+                    if (this.config[i].apiKey.length == 0)
+                        continue;
+                    else
+                        this.sendEmailWithService(i, this.config[i].apiKey, msg);
+                }
+            };
+            this.config = {
+                mailchimp: {
+                    apiKey: ''
+                },
+                sendgrid: {
+                    apiKey: ''
+                }
+            };
+            if (Object.values(config).length > 0)
+                this.config = Object.assign({}, this.config, config);
+            else
+                throw new Error('Config file should be an object with services');
+        }
     }
-}
-exports.default = MailService;
+    QuangLibrary.MailService = MailService;
+    // export default MailService
+})(QuangLibrary || (QuangLibrary = {}));
 //# sourceMappingURL=index.js.map
